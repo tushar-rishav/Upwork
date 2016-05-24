81879c2f7cf99b9d4db45083e95d1106073cf5d9
Tushar Gautam <tushar.rishav@gmail.com>
2016-05-24 20:02:16 +0530

    pyautogui: Update Swastika with Peace

--- pyautogui/draw_paint.py
+++ pyautogui/draw_paint.py
@@ -1,22 +1,30 @@
+import math
 import pyautogui
 import time
 
 time.sleep(5)
 
-# pyautogui.typewrite('Hello world!')
-pyautogui.click()
+cx, cy = pyautogui.size()
+cx /= 2
+cy /= 2
+radius = 100
+angle = 0
+omega = 0.3
 
-distance = 100
-moves = [
-	{'x':-1,'y':0},
-	{'x':0,'y':2},
-	{'x':-1,'y':0},
-	{'x':1,'y':0},
-	{'x':0,'y':-1},
-	{'x':-1,'y':0},
-	{'x':0,'y':-1},
-	{'x':0,'y':1},
-	{'x':2,'y':0},
-	{'x':0,'y':1}]	
-for move in moves:
-	pyautogui.dragRel(move['x']*distance, move['y']*distance, duration=0.2)
+# parametric equation of circle
+x = cx + radius * math.cos(angle)
+y = cy + radius * math.sin(angle)
+pyautogui.click(x,y)
+
+while 2*math.pi - angle >= 0.001:
+	angle = angle + omega
+	x = x + radius * omega * math.cos(angle + math.pi / 2)
+	y = y + radius * omega * math.sin(angle + math.pi / 2)
+	pyautogui.dragTo(x, y, duration=0.2)
+
+pyautogui.moveTo(cx, cy-radius, duration=0.2)
+pyautogui.dragTo(cx, cy+radius, duration=0.2)
+pyautogui.moveTo(cx, cy, duration=0.2)
+pyautogui.dragTo(cx - radius * math.cos(math.pi/4), cy + radius * math.sin(math.pi/4), duration=0.2)
+pyautogui.moveTo(cx, cy, duration=0.2)
+pyautogui.dragTo(cx + radius * math.cos(math.pi/4), cy + radius * math.sin(math.pi/4), duration=0.2)
